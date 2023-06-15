7c538f6f7c68142733f6d75ea9c043615d3057c8
zanson <zjl188318>
2023-06-15 12:00:04 +0800

    modify

---
+++ option.go
@@ -0,0 +1,30 @@
+package micro
+
+type OptionFun func(o *Option)
+
+//options
+type Option struct {
+	//service name
+	serviceName string
+	//version
+	version string
+	//
+	address string
+}
+
+func Name(name string) OptionFun {
+	return func(o *Option) {
+		o.serviceName = name
+	}
+}
+
+func Version(version string) OptionFun {
+	return func(o *Option) {
+		o.version = version
+	}
+}
+func Address(addr string) OptionFun {
+	return func(o *Option) {
+		o.address = addr
+	}
+}
