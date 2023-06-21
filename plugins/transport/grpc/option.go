392a4e4b63055679244df6a05c44630b9cb68333
zanson <zjl188318>
2023-06-21 09:49:02 +0800

    modify

---
+++ plugins/transport/grpc/option.go
@@ -0,0 +1,35 @@
+package grpc
+
+import "github.com/zjllib/go-micro/transport"
+
+type OptionFun func(o *Option)
+
+//options
+type Option struct {
+	//service name
+	serviceName string
+	//version
+	version string
+	//
+	address string
+
+	transport transport.ITransport
+}
+
+func Name(name string) OptionFun {
+	return func(o *Option) {
+		o.serviceName = name
+	}
+}
+
+func Version(version string) OptionFun {
+	return func(o *Option) {
+		o.version = version
+	}
+}
+
+func Address(addr string) OptionFun {
+	return func(o *Option) {
+		o.address = addr
+	}
+}
